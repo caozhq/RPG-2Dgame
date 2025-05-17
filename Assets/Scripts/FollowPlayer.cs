3ed6042a097eced9e76f324e43b2c4933e11794b
LucasCao <cao_zq@sina.com>
2025-05-17 19:16:46 +0800

    Initial commit
    Former-commit-id: 5bcd3dfe52270d6d4452d5845afbae2e5e8bdc0e

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    void LateUpdate()
+    {
+        
+        transform.position = new Vector3(player.position.x, player.position.y, -10f);
+    }
+}
